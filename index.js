feb5c0145d2dda27145e71d248fdca48474a996c
Francisco Jaime <fjjaime42@gmail.com>
2020-09-25 16:13:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const port = 8080;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('test')
+});
+
+app.listen(port, console.log(`Puerto 8080`));
