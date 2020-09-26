f0058ea2ffba0b489f905aff5f9f620c707452e1
noemi <noesanz_7@msn.com>
2020-09-26 14:37:37 +0200

    añado rutas y controladores de producto

---
+++ app/controllers/product.controller.js
@@ -0,0 +1,44 @@
+
+exports.listaProductos = (req, res) => {
+
+    // let sql = `SELECT * FROM products`;
+    // connection.query(sql, (err, result) => {
+    //     if (err) throw err;
+    //     res.json(result)
+    // })
+}
+
+exports.crearProducto = (req, res) => {
+
+   // connection.query(sql, (err, result) => {
+    //     if (err) throw err;
+    //     res.send('producto creado')
+    // })
+}
+
+
+exports.eliminarProducto = (req, res) => {
+   // let id_product = req.params.id_product;
+    // let sql = `DELETE FROM products WHERE id_products = ${id_product}`;
+
+    // connection.query(sql, (err, result) => {
+    //     if (err) throw err;
+    //     res.send('producto eliminado')
+    //})
+
+}
+
+
+
+exports.actualizarProducto = (req, res) => {
+   
+
+    // connection.query(sql, (err, result) => {
+    //     if (err) throw err;
+    //     res.send('Producto actualizado');
+    // })
+}
+
+
+
+
