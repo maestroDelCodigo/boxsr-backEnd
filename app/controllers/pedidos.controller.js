f0a6601bfec8b04ec32976d1afb26d398a18d31e
noemi <noesanz_7@msn.com>
2020-09-26 19:10:59 +0200

    terminamos de añadir controladores pedidos

---
+++ app/controllers/pedidos.controller.js
@@ -0,0 +1,36 @@
+
+
+exports.crearPedido = (req, res) => {
+
+  
+}
+exports.listaPedidos = (req, res) => {
+
+   
+}
+exports.buscarPedido= (req, res) => {
+   
+
+}
+exports.modificarPedido= (req, res) => {
+   
+
+}
+exports.estadoPedido= (req, res) => {
+   
+
+}
+
+
+
+exports.eliminarPedido= (req, res) => {
+   
+
+}
+
+
+
+
+
+
+
