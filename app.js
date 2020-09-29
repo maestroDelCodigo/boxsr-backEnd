55c3b49b5c00a2e8eae0593dc8013abe1d1f6610
noemi <noesanz__7@msn.com>
2020-09-29 18:37:28 +0200

    elimino ruta y controlador de cliente

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var categoriaRouter =  require ('./routes/categoria');
-var clienteRouter =  require ('./routes/cliente');
+
 var coleccionRouter =  require('./routes/coleccion');
 var pedidosRouter =  require('./routes/pedidos');
 var productRouter =  require('./routes/product');
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/categoria',categoriaRouter);
-app.use('/cliente', clienteRouter);
+
 app.use('/coleccion', coleccionRouter);
 app.use('/pedidos', pedidosRouter);
 app.use('/product', productRouter);
