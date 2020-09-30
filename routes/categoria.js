0fa0a2843238947f138a6e0e0b867e8c06ca4d4c
noemi <noesanz__7@msn.com>
2020-09-30 16:12:36 +0200

    eliminar categoria comentado

--- routes/categoria.js
+++ routes/categoria.js
@@ -4,18 +4,15 @@ var router = express.Router();
 
 const categoriaController = require ('../controllers/categoriaController')
 
-    // Create a new user
-    router.post('/categoria', categoriaController.crearCategoria);
-
-    //Retrieve all user
-    router.get('/categoria', categoriaController.listaCategorias);
-
-    router.get('/categoria/:categoriaId', categoriaController.buscarCategoria);
     
-    // Update a user with userId
-    router.post('/categoria/:categoriaId', categoriaController.modificarCategoria);
+    router.get('/', categoriaController.listaCategorias);
 
-    // Delete a user with usereId
-    router.post('/categoria/:categoriaId', categoriaController.borrarCategoria);
+    router.post('/crearCategoria', categoriaController.crearCategoria);
+
+    router.get('/buscarCategoria/:id', categoriaController.buscarCategoria);
+    
+    router.post('/modificarCategoria/:id', categoriaController.modificarCategoria);
+
+    // router.post('/borrarCategoria/:id', categoriaController.borrarCategoria);
     
     module.exports= router;
