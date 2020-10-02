f0ec27f6797a336d0ddc7a3d45401a72e7364d40
noemi <noesanz__7@msn.com>
2020-10-02 08:37:49 +0200

    rutas y controladores pedidos funcionando

--- controllers/pedidosController.js
+++ controllers/pedidosController.js
@@ -1,37 +1,92 @@
 
 const connection = require('../config/db');
-let sha1 = require('sha1');
+
 
 pedidosController = {};
 
 
 pedidosController.crearPedido = (req, res) => {
 
+let estado_pago=req.body.estado_pago;
+let forma_entrega=req.body.forma_entrega;
+let iva=req.body.iva;
+let total_pedido=req.body.total_pedido;
+let estado_preparacion=req.body.estado_preparacion;
+let fecha_pedido=req.body.fecha_pedido;
+let notas=req.body.notas;
+
+
+
+let sql = `INSERT INTO pedido (estado_pago,forma_entrega,iva,total_pedido,estado_preparacion,fecha_pedido,notas)
+ VALUES ('${estado_pago}','${forma_entrega}','${iva}','${total_pedido}', '${estado_preparacion}','${fecha_pedido}','${notas}')`;
+
+ connection.query( sql, (err, result)=>{
+     if(err) throw err;
+     res.send('Pedido creado');
+ })
+
   
 }
+
+
 pedidosController.listaPedidos = (req, res) => {
 
-   
+let sql= `select * from pedido`;
+
+connection.query(sql ,(err,result) => {
+    if(err) throw err;
+    res.send(result);
+    }) 
 }
+
+
 pedidosController.buscarPedido= (req, res) => {
-   
 
+    let sql=`SELECT * FROM pedido where pedido_id= ${req.params.id}`;
+
+    connection.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
 }
+
+
 pedidosController.modificarPedido= (req, res) => {
+
+    let pedido_id=req.params.id;
+
+    let estado_pago=req.body.estado_pago;
+    let forma_entrega=req.body.forma_entrega;
+    let iva=req.body.iva;
+    let total_pedido=req.body.total_pedido;
+    let estado_preparacion=req.body.estado_preparacion;
+    let fecha_pedido=req.body.fecha_pedido;
+    let notas=req.body.notas;
    
+    let sql = `UPDATE pedido SET estado_pago ='${estado_pago}', forma_entrega='${forma_entrega}',
+     iva='${iva}', total_pedido='${total_pedido}', estado_preparacion='${estado_preparacion}', fecha_pedido='${fecha_pedido}',notas= '${notas}'
+     WHERE pedido_id =${pedido_id}`;
 
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send('Pedido modificado')
+    })
 }
-pedidosController.estadoPedido= (req, res) => {
-   
-
-}
 
 
 
-pedidosController.eliminarPedido= (req, res) => {
-   
 
-}
+// pedidosController.eliminarPedido= (req, res) => {
+//     let pedido_id= req.params.id;
+
+//         let sql = `DELETE FROM pedido WHERE pedido_id = ${pedido_id}`;
+        
+//         connection.query(sql, (err, result) => {
+//             if (err) throw err;
+//             res.send('Pedido Eliminado');
+//         })
+
+// }
 
 
 
