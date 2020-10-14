883764620eaccbf9a3bf617656866ec3be919c58
Roger Mateu <59694834+rmateusemis@users.noreply.github.com>
2020-10-14 20:27:31 +0200

    stripe + submitPayment+ restarStock

---
+++ config/stripe.js
@@ -0,0 +1,23 @@
+const stripeSecretKey = 'sk_test_51Hc87kA0e2wVUT1FvUdpaTzuzw1EhedRXPZZvdW7KnB8bDgt3RIP9xvB91Mgnej9ZAU2HLWBu7E49uBaqe9NHfec000FXqUZJe'
+const stripe = require('stripe')(stripeSecretKey);
+
+const payment = async (req, res, next) => {
+  try {
+    const charge = await stripe.charges.create({
+      amount: 3000,
+      currency: 'eur',
+      source: req.body.id,
+
+    });
+
+    req.charge = charge;
+    next();
+  } catch (error) {
+    res.json(error);
+  }
+
+};
+
+
+module.exports = payment;
+
