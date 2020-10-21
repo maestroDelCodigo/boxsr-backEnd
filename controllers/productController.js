84298f866f63faed2df6bdb046291450ca48ee86
Roger Mateu <59694834+rmateusemis@users.noreply.github.com>
2020-10-21 12:53:44 +0200

    ok

--- controllers/productController.js
+++ controllers/productController.js
@@ -31,49 +31,48 @@ productController.getProducto = (req, res) => {
 
 productController.crearProducto = (req, res) => {
 
-let nombre=req.body.nombre;
-let tipo_producto=req.body.tipo_producto;
-let codigo_producto=req.body.codigo_producto;
-let peso=req.body.peso;
-let stock= req.body.stock;
-let deleted=req.body.deleted;
-let fecha_creacion=req.body.fecha_creacion;
-let precio= req.body.precio;
-let imagen = req.body.nombre_imagen.replace(/\s/g, '');
-let descripcion = req.body.descripcion;
-let descripcion_resumen = req.body.descripcion_resumen;
-let descripcion_sirve = req.body.descripcion_sirve;
-let descripcion_usa = req.body.descripcion_usa;
-let descripcion_ingredientes = req.body.descripcion_ingredientes;
-    
+    let nombre = req.body.nombre;
+    let tipo_producto = req.body.tipo_producto;
+    let codigo_producto = req.body.codigo_producto;
+    let peso = req.body.peso;
+    let stock = req.body.stock;
+    let deleted = req.body.deleted;
+    let fecha_creacion = req.body.fecha_creacion;
+    let precio = req.body.precio;
+    let imagen = req.body.nombre_imagen.replace(/\s/g, '');
+    let descripcion = req.body.descripcion;
+    let descripcion_resumen = req.body.descripcion_resumen;
+    let descripcion_sirve = req.body.descripcion_sirve;
+    let descripcion_usa = req.body.descripcion_usa;
+    let descripcion_ingredientes = req.body.descripcion_ingredientes;
+
 
     let sql = `INSERT INTO producto (nombre,tipo_producto,codigo_producto,peso,stock,deleted,fecha_creacion,precio,descripcion, descripcion_resumen, descripcion_sirve, descripcion_usa, descripcion_ingredientes) 
     VALUES ('${nombre}','${tipo_producto}', '${codigo_producto}',
     '${peso}','${stock}','${deleted}' ,'${fecha_creacion}',${precio}, '${descripcion}', '${descripcion_resumen}', '${descripcion_sirve}', '${descripcion_usa}', '${descripcion_ingredientes}')`;
-        
-    if(imagen)
-    {
+
+    if (imagen) {
         connection.query(sql, (err, resultProductos) => {
-                    if (err) throw err;                               
-                
-                    let lastId = resultProductos.insertId;                
-                                                    
-                    let sqlProductosImagen = `INSERT INTO imagen_producto (producto_id, path)
+            if (err) throw err;
+
+            let lastId = resultProductos.insertId;
+
+            let sqlProductosImagen = `INSERT INTO imagen_producto (producto_id, path)
                     VALUES ('${lastId}','${imagen}')`;
-                    
-                    connection.query(sqlProductosImagen, (err, result) => {                        
-                        if (err) {                                        
-                            res.status(500).json({
-                                message: err.message
-                            });
-                        }else{
-                            res.json('producto creado');
-                        }                     
-                       
-                    })                              
-                
+
+            connection.query(sqlProductosImagen, (err, result) => {
+                if (err) {
+                    res.status(500).json({
+                        message: err.message
+                    });
+                } else {
+                    res.json('producto creado');
+                }
+
             })
-    }else {
+
+        })
+    } else {
         connection.query(sql, (err, result) => {
             if (err) throw err;
             res.json('producto creado')
@@ -94,28 +93,28 @@ let descripcion_ingredientes = req.body.descripcion_ingredientes;
 // }
 
 productController.descatalogarProducto = (req, res) => {
-    let producto_id=req.params.id;
+    let producto_id = req.params.id;
     let descatalogarProducto = req.body.deleted;
-        if (descatalogarProducto == false){
-            let sql = `UPDATE  producto  SET deleted = 0
+    if (descatalogarProducto == false) {
+        let sql = `UPDATE  producto  SET deleted = 0
             WHERE producto_id=' ${producto_id}'`
-            connection.query(sql, (err, result) => {
-                  if (err) throw err;
-        res.json('Producto activo');
-            }) 
-           
-        }
-        else{
-            let sql = `UPDATE  producto  SET deleted = 1
-          WHERE producto_id= '${producto_id}'`
-          connection.query(sql, (err, result) => {
+        connection.query(sql, (err, result) => {
             if (err) throw err;
-        res.json('Producto inactivo');
-            })
-           
-        }
- 
- }
+            res.json('Producto activo');
+        })
+
+    }
+    else {
+        let sql = `UPDATE  producto  SET deleted = 1
+          WHERE producto_id= '${producto_id}'`
+        connection.query(sql, (err, result) => {
+            if (err) throw err;
+            res.json('Producto inactivo');
+        })
+
+    }
+
+}
 
 
 
@@ -123,14 +122,14 @@ productController.actualizarProducto = (req, res) => {
 
     let producto_id = req.params.id;
 
-    let nombre=req.body.nombre;
-    let tipo_producto=req.body.tipo_producto;
-    let codigo_producto=req.body.codigo_producto;
-    let peso=req.body.peso;
-    let stock= req.body.stock;
-    let deleted=req.body.deleted;
+    let nombre = req.body.nombre;
+    let tipo_producto = req.body.tipo_producto;
+    let codigo_producto = req.body.codigo_producto;
+    let peso = req.body.peso;
+    let stock = req.body.stock;
+    let deleted = req.body.deleted;
     // let fecha_creacion=req.body.fecha_creacion;
-    let precio=req.body.precio;
+    let precio = req.body.precio;
     let imagen = req.body.nombre_imagen.replace(/\s/g, '');
     let imagen_url = req.body.imagen_url;
 
@@ -139,57 +138,55 @@ productController.actualizarProducto = (req, res) => {
     let descripcion_sirve = req.body.descripcion_sirve;
     let descripcion_usa = req.body.descripcion_usa;
     let descripcion_ingredientes = req.body.descripcion_ingredientes;
-       
+
     let sql = `UPDATE producto SET nombre='${nombre}', tipo_producto='${tipo_producto}',
     codigo_producto='${codigo_producto}', peso='${peso}',stock='${stock}',deleted=${deleted}, precio='${precio}', descripcion = '${descripcion}' , descripcion_resumen = '${descripcion_resumen}', descripcion_sirve = '${descripcion_sirve}', descripcion_usa = '${descripcion_usa}', descripcion_ingredientes = '${descripcion_ingredientes}' WHERE producto_id=${producto_id}`;
- 
-    if(imagen)
-    {
+
+    if (imagen) {
         connection.query(sql, (err, resultProductos) => {
-                    if (err) throw err;     
-                    
-                    let sqlDeleteImage = `DELETE FROM imagen_producto WHERE producto_id = ${producto_id}`;                    
+            if (err) throw err;
 
-                    connection.query(sqlDeleteImage, (err, result) => {                        
-                        if (err) throw err;                                                                                  
-
-                        let sqlProductosImagen = `INSERT INTO imagen_producto (producto_id, path)
-                        VALUES ('${producto_id}','${imagen}')`;                      
-                        
-                        connection.query(sqlProductosImagen, (err, result) => {                        
-                            if (err) {                                        
-                                res.status(500).json({
-                                    message: err.message
-                                });
-                            }else{
-                                res.json('producto actualizado');
-                            }                     
-                        
-                        })                                                                                              
-                    })                                                               
-                
-            })
-            //no tengo imagen pero tenia antes, la tengo q borrar.
-    }else if(!imagen && imagen_url){
-
-        connection.query(sql, (err, resultProductos) => {
-            if (err) throw err;     
-            
             let sqlDeleteImage = `DELETE FROM imagen_producto WHERE producto_id = ${producto_id}`;
-           
-            connection.query(sqlDeleteImage, (err, result) => {  
-                if (err) {                                        
+
+            connection.query(sqlDeleteImage, (err, result) => {
+                if (err) throw err;
+
+                let sqlProductosImagen = `INSERT INTO imagen_producto (producto_id, path)
+                        VALUES ('${producto_id}','${imagen}')`;
+
+                connection.query(sqlProductosImagen, (err, result) => {
+                    if (err) {
+                        res.status(500).json({
+                            message: err.message
+                        });
+                    } else {
+                        res.json('producto actualizado');
+                    }
+
+                })
+            })
+
+        })
+        //no tengo imagen pero tenia antes, la tengo q borrar.
+    } else if (!imagen && imagen_url) {
+
+        connection.query(sql, (err, resultProductos) => {
+            if (err) throw err;
+
+            let sqlDeleteImage = `DELETE FROM imagen_producto WHERE producto_id = ${producto_id}`;
+
+            connection.query(sqlDeleteImage, (err, result) => {
+                if (err) {
                     res.status(500).json({
                         message: err.message
                     });
-                }else{
+                } else {
                     res.json('producto actualizado');
-                }  
+                }
             });
         });
     }
-    else
-    {
+    else {
         connection.query(sql, (err, result) => {
             if (err) throw err;
             res.json('producto actualizado')
